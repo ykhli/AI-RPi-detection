694da561d37ad6fc1536a5da80ecd59ac73f273f
Yoko Li <ykhli1013@gmail.com>
2024-02-27 14:34:31 -0800

    update

--- camera.py
+++ camera.py
@@ -1,13 +1,20 @@
 
-from picamera2 import Picamera2
-import time, datetime, os, logging
+from picamera2 import Picamera2, Preview
+import time, os, logging
+from datetime import datetime
+
+logging.basicConfig(level=logging.INFO) 
 
 picam2 = Picamera2()
+picam2.start()
+time.sleep(2)
+
+
 
 def capture_photo():
     try:
         take_photo()  # Call your take_photo function
-        logging.info("Image captured successfully")
+        logging.info("Image captured sauccessfully")
     except Exception as e:
         return e
     
@@ -19,6 +26,7 @@ def take_photo():
         image_name = f'{timestamp}.jpg'
         current_dir = os.path.dirname(__file__)
         static_dir = os.path.join(current_dir, 'static')
+        logging.info(f"Capturing image. Path: {static_dir}")
         filepath = os.path.join(static_dir, image_name)
         request = picam2.capture_request()
         request.save("main", filepath)
