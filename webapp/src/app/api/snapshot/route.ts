ac825d28b59cfb21aff185bd3b07bd9d0d99189e
Yoko Li <ykhli1013@gmail.com>
2024-03-07 07:37:18 -0800

    logic for downloading image + sending to OAI (#8)
    * logic for downloading image + sending to OAI
    
    * update package.json lock

---
+++ webapp/src/app/api/snapshot/route.ts
@@ -0,0 +1,53 @@
+import { describeImage } from "@/app/utils";
+import {
+  GetObjectCommand,
+  ListObjectsV2Command,
+  S3Client,
+} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import OpenAI from "openai";
+
+export const dynamic = "force-dynamic";
+const client = new S3Client();
+
+export type TigrisObject = {
+  displayName: string;
+  key: string;
+  urlSlug: string;
+};
+
+export type FilesResponse = Array<TigrisObject>;
+
+export async function POST() {
+  const listObjectsV2Command = new ListObjectsV2Command({
+    Bucket: process.env.BUCKET_NAME,
+    Prefix: `${process.env.COLLAGE_FOLER_NAME!}/`,
+  });
+  const resp = await client.send(listObjectsV2Command);
+  if (!resp.Contents || resp.Contents.length === 0) {
+    console.log("No files found.");
+    return;
+  }
+
+  const latestFile = resp.Contents.sort(
+    (a: any, b: any) => b.LastModified - a.LastModified
+  )[0];
+
+  if (!latestFile) {
+    console.log("No file found.");
+    return;
+  }
+
+  const getObjectCommand = new GetObjectCommand({
+    Bucket: process.env.BUCKET_NAME,
+    Key: latestFile.Key,
+  });
+
+  const url = await getSignedUrl(client, getObjectCommand, {
+    expiresIn: 3600,
+  });
+
+  const aiResponse = await describeImage(url);
+
+  return Response.json(aiResponse);
+}
---
+++ webapp/src/app/utils.ts
@@ -0,0 +1,34 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env["OPENAI_API_KEY"], // This is the default and can be omitted
+});
+
+export async function describeImage(url: string) {
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: `
+              You are an AI assistant that can help me detect if there is a cat in the picture and what the cat is doing.
+           `,
+      },
+      {
+        role: "user",
+        content: [
+          {
+            type: "text",
+            text: `These are frames a camera stream consist of one to many pictures. Generate a compelling description of the image or a sequence of images: "`,
+          },
+          {
+            type: "image_url",
+            image_url: { url },
+          },
+        ],
+      },
+    ],
+    model: "gpt-4-vision-preview",
+    max_tokens: 2048,
+  });
+  return chatCompletion.choices[0].message;
+}
