d8696bc223f6c99c701534a3f0ab448cbc385ef2
Yoko Li <ykhli1013@gmail.com>
2024-04-29 07:20:56 +0100

    langchain implementation

--- camera.py
+++ camera.py
@@ -1,8 +1,7 @@
 
-from picamera2 import Picamera2, Preview
-import time, os, logging
+from picamera2 import Picamera2
+import time, os, logging, getpass
 from datetime import datetime
-from openai import OpenAI
 import base64
 import cv2
 import numpy as np
@@ -10,9 +9,14 @@ from elevenlabs import generate, play, set_api_key, save, voices
 from dotenv import load_dotenv
 import resend
 import json
+from langchain_core.tools import tool
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+
 
 load_dotenv()
 
+llm = ChatOpenAI(model="gpt-4-vision-preview", max_tokens=500)
 logging.basicConfig(level=logging.INFO) 
 save_location='static'
 save_dir = os.path.join(os.getcwd(), save_location)
@@ -24,13 +28,42 @@ if (os.environ.get("ELEVEN_API_KEY") != None):
 resend.api_key = os.environ.get("RESEND_API_KEY")
 print(f"USE_ELEVEN: {USE_ELEVEN}")
 
-openAI = OpenAI()
-
 picam2 = Picamera2()
 picam2.start()
 time.sleep(2)
 requestPrompt = os.environ.get("REQUEST_PROMPT")
 lastEmailTs = None
+
+@tool
+def send_email(detected, description, filePath):
+    """
+    send an email based on if a cat was detected in the response. 
+
+    There are three parameters: 
+    - detected: Boolean. Is the object detected?
+    - description: string. Description of the scene
+    - filePath: string. You don't need to worry about this one. 
+
+    """
+    global lastEmailTs
+
+    if (detected == False):
+        return
+    f = open(filePath, "rb").read()
+    params = {
+        "from": os.environ.get("FROM_EMAIL"),
+        "to": [os.environ.get("TO_EMAIL")],
+        "subject": "AI dection!",
+        "html": f"<strong>{description}</strong>",
+        "attachments": [{"content": list(f), "filename": "image.jpg"}],
+    }
+    email = resend.Emails.send(params)
+    if (email['id']!=None):
+        lastEmailTs = datetime.now()
+    logging.info(f"Email sending status: {email}, {lastEmailTs}")
+
+tools = [send_email]
+llm_with_tools = llm.bind_tools(tools)
 
 def play_audio(text):
     try: 
@@ -46,58 +79,22 @@ def play_audio(text):
         print(f"Error generating and playing audio: {e}")
 
 
-def describe_image(base64_images):
-    # for testing
-    logging.info(f"base64_images: {len(base64_images)}")
-    response = openAI.chat.completions.create(
-        model="gpt-4-vision-preview",
-        messages=[
-            {
-                "role": "user",
-                "content": [
-                   requestPrompt, *map(lambda x: {"image": x, "resize": 768}, base64_images),
-                    # {"type": "text", "text": "Describe this image"},
-                    # {
-                    #     "type": "image_url",
-                    #     "image_url": f"data:image/jpeg;base64,{base64_image}",
-                    # },
-                ],
-            },
-        ],
-        max_tokens=500,
-        tools=[{
-            "type": "function", 
-            "function": {
-                "name": "send_email",
-                "description": "send an email based on if a cat was detected in the response", 
-                "parameters": {
-                    "type": "object", 
-                    "properties": {
-                        "detected": {
-                            "type": "boolean",
-                            "description": "if a cat was detected in the response"
-                        },
-                        "description": {
-                            "type": "boolean",
-                            "description": "if a cat was detected in the response, the description given by the model"
-                        }
-                    },
-                    "required": ["detected", "description"]
+def describe_image(collageFilePath):
+    base64 = encode_image(collageFilePath)
+    result = llm_with_tools.invoke(
+        [HumanMessage(
+            content = [
+                 {"type": "text", "text": requestPrompt},
+                 {"type": "image_url", 
+                  "image_url": 
+                    {"url": f"data:image/jpeg;base64,{base64}"
+                    }
                 }
-            }
-        }]
+        ])]
     )
-    print("response:", response.choices[0].message)
-    return response.choices[0].message
+    print('langchain result: ', result)
+    return result
 
-
-def capture_photo():
-    try:
-        filePath = take_photo()  # Call your take_photo function
-        logging.info("Image captured sauccessfully")
-        return filePath
-    except Exception as e:
-        return e
     
 def encode_image(image_path):
     while True:
@@ -155,52 +152,38 @@ def save_image_collage(base64_images):
     logging.info(f"Montage saved successfully. Path: {file_path}")
     return file_path
 
-def send_email(detected, description, filePath):
-    global lastEmailTs
-
-    if (detected == False):
-        return
-    f = open(filePath, "rb").read()
-    params = {
-        "from": os.environ.get("FROM_EMAIL"),
-        "to": [os.environ.get("TO_EMAIL")],
-        "subject": "AI dection!",
-        "html": f"<strong>{description}</strong>",
-        "attachments": [{"content": list(f), "filename": "image.jpg"}],
-    }
-    email = resend.Emails.send(params)
-    if (email['id']!=None):
-        lastEmailTs = datetime.now()
-    logging.info(f"Email sending status: {email}, {lastEmailTs}")
 
 def main():
     base64Frames = []
-    numOfFrames = 5
+    #TODO
+    numOfFrames = 2
     availableFunctions = {"send_email": send_email}
     global lastEmailTs
 
     while True:
-        filePath = capture_photo()
+        filePath = take_photo()
         base64_image = encode_image(filePath)
         if len(base64Frames) < numOfFrames:
             base64Frames.append(base64_image)
         else:
             collageFilePath = save_image_collage(base64Frames)
-            aiResponse = describe_image(base64Frames)
+            aiResponse = describe_image(collageFilePath)
+            if (USE_ELEVEN): 
+                play_audio(aiResponse.content)
             if (aiResponse.tool_calls):
                 for tool_call in aiResponse.tool_calls:
                     try:
-                        print(tool_call.function)
-                        args = json.loads(tool_call.function.arguments)
+                        functionToCall = tool_call['name']
+                        args = tool_call['args']
                         print('lastEmailTs', lastEmailTs)
                         if (lastEmailTs == None or (datetime.now() - lastEmailTs).seconds > 60):
-                            extra_param = {"filePath": collageFilePath}
-                            availableFunctions[tool_call.function.name](**args, **extra_param)
+                            args["filePath"] = collageFilePath
+                            selectedFunction = availableFunctions[functionToCall]
+                            selectedFunction.invoke(args)
                     except Exception as e:
                         print(f"An error occurred while calling the function: {e}")
-
-            if (USE_ELEVEN): 
-                play_audio(aiResponse.content)
+            
+            
             base64Frames = []
 
         time.sleep(2)
